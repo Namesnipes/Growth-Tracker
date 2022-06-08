0af73890e30b7aed138c7ab935311d49ba03267c
MooMan <36575231+Namesnipes@users.noreply.github.com>
2022-06-08 14:56:45 -0700

    Template of the extension

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert('hi!')
+var homeButton = document.getElementById("homeButton")
+var calendarButton = document.getElementById("calendarButton")
+var journalButton = document.getElementById("journalButton")
+var title = document.getElementById("title")
+
+
+function goToHomePage(){
+  title.textContent = "Home Page :D"
+}
+
+function goToCalendarPage(){
+  title.textContent = "Calendar Page :D"
+}
+
+function goToJournalPage(){
+  title.textContent = "Journal Page :D"
+}
+
+homeButton.addEventListener("click", goToHomePage)
+calendarButton.addEventListener("click", goToCalendarPage)
+journalButton.addEventListener("click", goToJournalPage)
