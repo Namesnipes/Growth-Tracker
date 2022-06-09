98fb251d72a56be87a7390ac4c68f9181ad2a388
MooMan <36575231+Namesnipes@users.noreply.github.com>
2022-06-08 19:43:05 -0700

    Displays todays date, comments

--- popup.js
+++ popup.js
@@ -1,23 +1,31 @@
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 var homeButton = document.getElementById("homeButton")
 var calendarButton = document.getElementById("calendarButton")
 var journalButton = document.getElementById("journalButton")
 var textboxSubmitButton = document.getElementById("textboxSubmitButton")
 
+var dateString;
+var yearNumber;
+
+//SWITCH PAGES
 function goToHomePage(){
-  title.textContent = "Home Page :D"
+  title.textContent = dateString + " - Home"
 }
 
 function goToCalendarPage(){
-  title.textContent = "Calendar Page :D"
+  title.textContent = dateString + " - Calendar"
 }
 
 function goToJournalPage(){
-  title.textContent = "Journal Page :D"
+  title.textContent = dateString + " - Journal"
 }
 
+
+//DATA IO
 function saveTextBox(){
   var text = textbox.value
-  console.log(text)
+  console.log("Saving: " + text)
   setData(text)
 }
 
@@ -37,16 +45,40 @@ function getData(key,callback){
   })
 }
 
+//SETTERS
 function updateTextBox(text){
   textbox.value = text
 }
 
+function updateDate(){
+  var now = new Date()
+  var dateNumber = now.getDate()
+  var monthString = month[now.getMonth()]
+  yearNumber = now.getFullYear()
+
+  dateString = monthString + " " + dateNumber + nth(dateNumber)
+
+  title.textContent = dateString
+  subtitle.textContent = yearNumber
+
+}
+
 function pageLoaded(){
   getData("text",updateTextBox)
+  updateDate()
+}
+
+//HELPERS
+function nth(i){
+  if(i > 3 && i < 21) return "th"
+  if(i % 10 == 3) return "rd"
+  if(i % 10 == 2) return "nd"
+  if(i % 10 == 1) return "st"
+  return "th"
 }
 
 
-
+//EVENTS
 textboxSubmitButton.addEventListener("click", saveTextBox)
 homeButton.addEventListener("click", goToHomePage)
 calendarButton.addEventListener("click", goToCalendarPage)
